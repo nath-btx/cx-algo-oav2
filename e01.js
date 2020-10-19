c46dc0712ab5f308318b859f64f9d71ae5a56e7f
nath-btx <nathan.bertiaux@hotmail.fr>
2020-10-19 08:48:23 +0200

    exercice 1 - Tri à bulle

---
+++ e01.js
@@ -0,0 +1,28 @@
+// Bubble sorting
+
+// i parcours chaîne
+// Comparer i à i + 1
+// à chaque itération la plus grande valeur se retrouve à la fin
+
+function Bubblesort(arr)
+{
+    
+    let len = arr.length;
+    for (let i = 0; i < arr.length; i++)
+    {
+        for (let j = 0; j < len; j++)  
+        {
+            if (arr[j] > arr[j+1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+        len -= 1;      
+    }   
+    return arr;
+}
+
+arr = [4,6,3,2,0,9,5,2,1];
+console.log(Bubblesort(arr))
